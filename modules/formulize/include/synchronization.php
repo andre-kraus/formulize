7010baa43c1ff3952274017c36d690687fc42b13
Lee Glendenning <glen9190@mylaurier.ca>
2016-03-21 16:52:20 -0400

    Add cacheExportFile function

--- modules/formulize/include/synchronization.php
+++ modules/formulize/include/synchronization.php
@@ -398,7 +398,18 @@
         deleteDir($csvFolderPath); // clean up temp folder and CSV files
         return array( "success" => $successfulImport);
     }
-    
+
+    /*
+     * cacheExportFile function moves the zip being imported to the cache directory
+     * and renames the zip by the current session id. This function is the last step of
+     * the first import page on the front end
+     * 
+     * param  archivePath       String path to archive file
+     */
+    function cacheExportFile($archivePath){
+        // copies the archive file to the cache folder and renames the copied file to the session id
+        rename($archivePath, XOOPS_ROOT_PATH . "\\modules\\formulize\\cache\\".session_id().".zip");
+    }
     
     /*
      * printArr utility function prints each element of given 1-D String array with comma separator
